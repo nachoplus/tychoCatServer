9ad1764f9255fe493804b2dec4231c6c59d3a782
nachoplus <mas.ignacio@gmail.com>
2020-01-24 09:03:10 +0100

    Splited bootstrap into: bootstrap and downloader

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ COPY requirements.txt /app/
 RUN pip3 install -r  /app/requirements.txt 
 COPY / /app
 WORKDIR /app
+RUN ["python3","/app/bootstrap.py"]
 ENTRYPOINT ["python3"]
 CMD  ["tychoCatServer.py"]
 
--- downloader.py
+++ downloader.py
@@ -10,7 +10,7 @@ from config import *
 import logging
 
 # Create a custom logger
-logger = logging.getLogger('bootstrap')
+logger = logging.getLogger('dowloader')
 
 
 def exe(cmd):
@@ -20,57 +20,39 @@ def exe(cmd):
         return res
 
 
-def ucac4src():
-        org_path=os.getcwd()
-        os.chdir('ucac4.src')
-        logger.info("Installing ucac4 swig rutines")
-        cmd='./mkswig.sh'
-        exe(cmd)
-        os.chdir(org_path)
-
-def lunar():
-        logger.info("Installing integrat from project pluto:")
-        org_path=os.getcwd()
-
-        os.chdir('lunar')
-        cmd='make  clean'
-        logger.info("%s",exe(cmd))
-        cmd='make '
-        logger.info("%s",exe(cmd))
-        cmd='make install'
-        logger.info("%s",exe(cmd))
-        os.chdir(org_path)
-
-        os.chdir('jpl_eph')
-        cmd='make  clean'
-        logger.info("%s",exe(cmd))
-        cmd='make '
-        logger.info("%s",exe(cmd))
-        cmd='make install'
-        logger.info("%s",exe(cmd))
-        os.chdir(org_path)
-
-        os.chdir('lunar')
-        cmd='make integrat'
-        logger.info("%s",exe(cmd))
-        cmd='chmod a+x integrat'
-        logger.info("%s",exe(cmd))
-        cmd='cp integrat ..'
-        logger.info("%s",exe(cmd))
-        os.chdir(org_path)
+def ucac4files():
+        logger.info("Step 1. ==============================")
+        logger.info("Downloading UCAC4 catalog files:")
+        logger.info("visit http://cdsarc.u-strasbg.fr/viz-bin/Cat?I/322A for catalog description")
+        cfg=dict(config.items("UCAC4"))
+        path=cfg['datadir']
+        if not os.path.exists(path+"/UCAC4.finished"):
+                if not os.path.exists(path):
+                    logger.info("Creating UCAC4 catalog dir:%s",path)
+                    os.makedirs(path)
+                logger.info("Downloading UCAC4 files to:%s",path)
+                call_list=["wget","-cr","-nH","--cut-dirs=5","ftp://cdsarc.u-strasbg.fr/pub/cats/I/322A/UCAC4/*","-P",path]
+                p=call(call_list)
+                logger.info("%s",exe("touch "+path+"/UCAC4.finished"))
+        else:
+                logger.info("UCAC4 files already download")
 
 
-def pyephem():
-        #Not needed. Now upstream is fixed.
-        logger.info("Installing modified version of pyephem")
-        logger.info("Not needed. Now upstream is fixed.")
-        return
-        org_path=os.getcwd()
-        #os.chdir('pyephem/pyephem-3.7.5.1')
-        os.chdir('ephem-3.7.6.0')
-        cmd='python3 setup.py install'
-        logger.info("%s",exe(cmd))
-        os.chdir(org_path)
+def jplEph():
+        pkg='ftp://ssd.jpl.nasa.gov/pub/eph/planets/Linux/de430/linux_p1550p2650.430'
+        cfg=dict(config.items("MPCORB"))
+        path=os.path.dirname(os.path.abspath(cfg['de_jpl']))
+        file_name = pkg.split('/')[-1]
+        logger.info("Installing JPL ephem file:%s",file_name)
+        if not os.path.exists(path):
+                logger.info("Creating JPL Ephem dir:%s",path)
+                os.makedirs(path)
+        if not os.path.isfile(path+'/'+file_name):
+                logger.info("Dowloading:%s",pkg)
+                urlretrieve(pkg,path+'/'+file_name)
+        else:
+                logger.info("%s allready downloaded",file_name)
+
 
 if __name__ == '__main__':
         logger.warning("================================================")
@@ -79,10 +61,8 @@ if __name__ == '__main__':
         logger.warning("CHECK your data_dir in config/main.cfg file!!!")
         logger.warning("================================================")
         logger.warning("")
-        ucac4src()
-        lunar()
-        #Not needed. Now upstream is fixed.
-        #pyephem()
+        ucac4files()
+        jplEph()
         cfg=dict(config.items("MPCORB"))
         path=cfg['base_dir']
         logger.warning("================================================")

d898013c2c47cfb70daa8d231bde69556e8e33e8
nachoplus <mas.ignacio@gmail.com>
2020-01-27 00:24:46 +0100

    Fix swig python version ussing pkg.config

--- code/ucac4.src/mkswig.sh
+++ code/ucac4.src/mkswig.sh
@@ -1,5 +1,5 @@
 swig -python ucac4.i
-gcc -c ucac4.c ucac4_wrap.c -I/usr/include/python3.7 -fPIC
-ld -shared ucac4.o ucac4_wrap.o -o _ucac4.so
+gcc -c ucac4.c ucac4_wrap.c  -fPIC $(pkg-config --cflags python3)
+ld -shared ucac4.o ucac4_wrap.o -o _ucac4.so $(pkg-config --libs python3)
 cp _ucac4.so ..
 cp ucac4.py ..

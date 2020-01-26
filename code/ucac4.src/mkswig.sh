001f93ecd57ced78af972fe61b43ad23fc576a2d
nachoplus <mas.ignacio@gmail.com>
2020-01-27 00:35:39 +0100

    added -fno-stack-protector to cflags to avoid linking error in mkswig.sh

--- code/ucac4.src/mkswig.sh
+++ code/ucac4.src/mkswig.sh
@@ -1,5 +1,5 @@
 swig -python ucac4.i
-gcc -c ucac4.c ucac4_wrap.c  -fPIC $(pkg-config --cflags python3)
+gcc -c ucac4.c ucac4_wrap.c  -fPIC $(pkg-config --cflags python3) -fno-stack-protector
 ld -shared ucac4.o ucac4_wrap.o -o _ucac4.so $(pkg-config --libs python3)
 cp _ucac4.so ..
 cp ucac4.py ..
